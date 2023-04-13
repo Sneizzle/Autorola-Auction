a8c880bba0d0fc0f974e458a010282c74565767a
John Kiærbye Lange <jkla40320@edu.ucl.dk>
2023-04-13 09:58:24 +0200

    First Stable Version

---
+++ src/pages/about.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+	return (
+		<div>
+			<p>
+				AUTOROLA Group is a global leader in online remarketing and automotive IT solutions for professional used car and fleet management. AUTOROLA Group, is headquartered in Denmark and the company has currently approximately 476 employees and subsidiaries in 19 countries across Europe, North America, Latin America and Asia-Pacific.
+
+				AUTOROLA Group is committed to a constant renewal process throughout the digital transformation. The company adapts constantly to the needs of our customers, always from an online perspective, to achieve success in the proper commercialisation of used vehicles coming from de-fleeting processes.
+
+				AUTOROLA Group is divided into three global business units:
+			</p>
+		</div>
+	);
+};
+
+export default About;
---
+++ src/pages/details.js
@@ -0,0 +1,40 @@
+import Carousel from "../components/carousel/Carousel";
+const Details = () => {
+	const slides = [
+		{ url: "http://localhost:3000/image-1.jpg", title: "First" },
+		{ url: "http://localhost:3000/image-2.jpg", title: "Second" },
+		{ url: "http://localhost:3000/image-3.jpg", title: "Third" },
+		{ url: "http://localhost:3000/image-4.jpg", title: "First" },
+		{ url: "http://localhost:3000/image-5.jpg", title: "First" },
+		{ url: "http://localhost:3000/image-6.jpg", title: "First" },
+		{ url: "http://localhost:3000/image-7.jpg", title: "First" },
+		{ url: "http://localhost:3000/image-8.jpg", title: "First" },
+		{ url: "http://localhost:3000/image-9.jpg", title: "First" },
+		{ url: "http://localhost:3000/image-10.jpg", title: "First" },
+		{ url: "http://localhost:3000/image-11.jpg", title: "First" },
+		{ url: "http://localhost:3000/image-12.jpg", title: "First" },
+		{ url: "http://localhost:3000/image-13.jpg", title: "First" },
+		{ url: "http://localhost:3000/image-14.jpg", title: "First" },
+		{ url: "http://localhost:3000/image-15.jpg", title: "First" },
+		{ url: "http://localhost:3000/image-16.jpg", title: "First" },
+		{ url: "http://localhost:3000/image-17.jpg", title: "First" },
+	];
+	
+	const containerStyles = {
+		width: '500px',
+		height: '280px',
+		margin: '0 auto',
+	};
+	return (
+		<div>
+			<h1 style={{textAlign: "center"}}>Autorola	</h1>
+			<div style={containerStyles}>
+				<Carousel slides={slides} />
+			</div>
+
+		</div>
+	);
+};
+
+
+export default Details;
